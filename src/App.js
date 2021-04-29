bcb67ce3cfa7d128862ba2454d74d540bf93be62
Ashley Finafrock <v-asho@microsoft.com>
2021-04-29 08:31:52 -0700

    Abstracted axios into http service

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
 import React, { Component } from "react";
-import axios from "axios";
+import http from "./services/httpService";
 import "./App.css";
-
-axios.interceptors.response.use(null, (error) => {
-  console.log("response interceptor");
-  const expectedError =
-    error.response &&
-    error.response.status >= 400 &&
-    error.response.status <= 500;
-
-  if (!expectedError) {
-    console.log(error);
-    alert("An unexpected error occurrred.");
-  }
-
-  return Promise.reject(error);
-});
-
-const apiEndpoint = "https://jsonplaceholder.typicode.com/posts";
+import config from "./config.json";
 
 class App extends Component {
   state = {
@@ -25,13 +9,13 @@ class App extends Component {
   };
 
   async componentDidMount() {
-    const { data: posts } = await axios.get(apiEndpoint);
+    const { data: posts } = await http.get(config.apiEndpoint);
     this.setState({ posts });
   }
 
   handleAdd = async () => {
     const obj = { title: "a", body: "b" };
-    const { data: post } = await axios.post(apiEndpoint, obj);
+    const { data: post } = await http.post(config.apiEndpoint, obj);
     const posts = [post, ...this.state.posts];
 
     this.setState({ posts });
@@ -39,7 +23,7 @@ class App extends Component {
 
   handleUpdate = async (post) => {
     post.title = "UPDATED";
-    await axios.put(`${apiEndpoint}/${post.id}`, post);
+    await http.put(`${config.apiEndpoint}/${post.id}`, post);
 
     const posts = [...this.state.posts];
     const index = posts.indexOf(post);
@@ -55,14 +39,10 @@ class App extends Component {
     this.setState({ posts });
 
     try {
-      await axios.delete(`${apiEndpoint}/${post.id}`);
+      await http.delete(`sdfifw${config.apiEndpoint}/${post.id}`);
     } catch (error) {
       if (error.response && error.response.status === 404)
         alert("This post has already been deleted");
-      else {
-        console.log(error);
-        alert("An unexpected error occurred.");
-      }
 
       this.setState({ posts: originalPosts });
     }
